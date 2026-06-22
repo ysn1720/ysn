b2ff08d793cbe36dc54c8ad4a78cb48fc4b0ed3d
ysn1720 <168485933+ysn1720@users.noreply.github.com>
2026-06-22 13:31:49 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -355,7 +355,7 @@ media.forEach(el => {
 
 // p47表示状態を更新する関数
 function updateP47Visibility() {
-    if (window.innerWidth > 500) return;  // モバイルのみ
+    
     const p47 = document.querySelector('.p47');
     if (!p47) return;
     const allHidden = media.every(el => el.classList.contains('hide'));
